cb5dd3106fb93eb9841927e267497b7bb5b11461
iosアカデミア <yutamac2512@icloud.com>
2025-03-22 21:16:46 +0900

    パスとシェイプの描画

---
+++ PracticeSwiftUI/PracticeSwiftUI/SwiftUiTutorial/No2/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  PracticeSwiftUI
+//
+//  Created by Engineer MacBook Air on 2025/03/22.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
