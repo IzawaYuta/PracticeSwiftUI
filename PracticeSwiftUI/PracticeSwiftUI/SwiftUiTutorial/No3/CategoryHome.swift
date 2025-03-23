f75f8bf512344b7186f7ce7ccb13e89f22165c4e
iosアカデミア <yutamac2512@icloud.com>
2025-03-23 11:20:30 +0900

    複雑なインターフェースを構成する　途中

---
+++ PracticeSwiftUI/PracticeSwiftUI/SwiftUiTutorial/No3/CategoryHome.swift
@@ -0,0 +1,38 @@
+//
+//  CategoryHome.swift
+//  PracticeSwiftUI
+//
+//  Created by Engineer MacBook Air on 2025/03/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @Environment(ModelData.self) var modelData
+    
+    var body: some View {
+        NavigationSplitView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .navigationTitle("Featured")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+        .environment(ModelData())
+}
