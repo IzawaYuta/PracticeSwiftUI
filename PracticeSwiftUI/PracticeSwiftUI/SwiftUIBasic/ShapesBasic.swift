345e9a3f9ee9458fe290a19221f609ce3215aa5e
iosアカデミア <yutamac2512@icloud.com>
2025-04-07 16:03:52 +0900

    ShapesBasic

--- PracticeSwiftUI/PracticeSwiftUI/SwiftUIBasic/ShapesBasic.swift
+++ PracticeSwiftUI/PracticeSwiftUI/SwiftUIBasic/ShapesBasic.swift
@@ -9,14 +9,20 @@ import SwiftUI
 
 struct ShapesBasic: View {
     var body: some View {
-        ZStack {
+        VStack {
             Circle()
+                .frame(width: 200, height: 100)
+            Ellipse()
+                .frame(width: 200, height: 100)
+            Capsule(style: .circular)
+                .frame(width: 200, height: 100)
+            Rectangle()
             //            .fill(Color.green)
             //            .foregroundColor(.pink)
             //            .stroke(Color.red, lineWidth: 30)
             //            .stroke(Color.red, style: StrokeStyle(lineWidth: 20, lineCap: .round, dash: [10]))
             //            .trim(from: 0.2, to: 1.0)
-            Ellipse()
+                .frame(width: 200, height: 100)
         }
     }
 }
