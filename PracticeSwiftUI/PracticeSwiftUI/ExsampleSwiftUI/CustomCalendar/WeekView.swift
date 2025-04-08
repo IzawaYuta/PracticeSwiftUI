f55a9e06dea9999597a16cc13895a773f10186f9
iosアカデミア <yutamac2512@icloud.com>
2025-04-08 14:41:02 +0900

    コミット

---
+++ PracticeSwiftUI/PracticeSwiftUI/ExsampleSwiftUI/CustomCalendar/WeekView.swift
@@ -0,0 +1,18 @@
+//
+//  WeekView.swift
+//  PracticeSwiftUI
+//
+//  Created by Engineer MacBook Air on 2025/04/08.
+//
+
+import SwiftUI
+
+struct WeekView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeekView()
+}
