05e2efaba2c8054d3c0c0a03f99a3b197a90df15
iosアカデミア <yutamac2512@icloud.com>
2025-04-08 18:06:04 +0900

    コミット

---
+++ PracticeSwiftUI/PracticeSwiftUI/ExsampleSwiftUI/CustomCalendar/CustomClendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomClendarApp.swift
+//  PracticeSwiftUI
+//
+//  Created by Engineer MacBook Air on 2025/04/08.
+//
+
+import SwiftUI
+
+@main
+struct CustomClendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CustomCalendarContentView()
+        }
+    }
+}
---
+++ PracticeSwiftUI/PracticeSwiftUI/ExsampleSwiftUI/CustomCalendar/MonthView.swift
@@ -0,0 +1,31 @@
+//
+//  MonthView.swift
+//  PracticeSwiftUI
+//
+//  Created by Engineer MacBook Air on 2025/04/08.
+//
+
+import SwiftUI
+
+struct MonthView: View {
+    
+    let month: Month
+    let dragProgress: CGFloat
+    
+    @Binding var focused: Week
+    @Binding var selectedDate: Date?
+    
+    var body: some View {
+        VStack(spacing: .zero) {
+            ForEach(month.weeks) { week in
+                WeekView(week: week, dragProgress: dragProgress,selectedDate: $selectedDate)
+                    .opacity(focused == week ? 1 : dragProgress)
+                    .frame(height: Constants.monthHeight / CGFloat(month.weeks.count))
+            }
+        }
+    }
+}
+
+#Preview {
+    MonthView(month: .init(from: .now, order: .current), dragProgress: 1, focused: .constant(.current), selectedDate: .constant(nil))
+}
